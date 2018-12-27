489f923c39745364826eb2e6d9a12ace7c788ad9
Ohad Cohen <ohadc3@gmail.com>
2018-12-28 01:06:46 +0200

    Created draw function in EdgeThresholdsGraphWithTerminals

--- activation_edge_cover.py
+++ activation_edge_cover.py
@@ -1,34 +1,5 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-
 from graph_factory import create_edge_thresholds_graph_with_terminals
 
 graph = create_edge_thresholds_graph_with_terminals()
 
-pos = graph.get_layout()
-
-G = graph.get_graph()
-nodes_drawing = nx.draw_networkx_nodes(G, pos, nodelist=graph.get_terminals(), node_color='r')
-nodes_drawing.set_edgecolor('black')
-nodes_drawing = nx.draw_networkx_nodes(G, pos, nodelist=graph.get_nodes()-graph.get_terminals(), node_color='w')
-nodes_drawing.set_edgecolor('black')
-
-nx.draw_networkx_edges(G, pos)
-
-edge_labels = nx.get_edge_attributes(G, 'tu')
-nx.draw_networkx_edge_labels(G, pos,
-                             edge_labels=edge_labels,
-                             label_pos=0.1,
-                             rotate=False)
-
-edge_labels = nx.get_edge_attributes(G, 'tv')
-nx.draw_networkx_edge_labels(G, pos,
-                             edge_labels=edge_labels,
-                             label_pos=0.9,
-                             rotate=False)
-
-plt.axis('off')
-
-plt.savefig("output/activation_edge_cover.png")
-
-plt.show()
+graph.draw('red', 'white', 'output/activation_edge_cover.png')
--- edge_thresholds_graph_with_terminals.py
+++ edge_thresholds_graph_with_terminals.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 from typing import List, Dict, Tuple
 
 
@@ -26,3 +27,36 @@ class EdgeThresholdsGraphWithTerminals(object):
 
     def get_layout(self, seed=0):
         return nx.spring_layout(self.get_graph(), seed=seed)
+
+    def draw(self, terminal_color='r', nonterminal_color='w', save=None):
+        G = self.get_graph()
+        pos = self.get_layout()
+
+        nodes_drawing = nx.draw_networkx_nodes(G, pos, nodelist=self.get_terminals(),
+                                               node_color=terminal_color)
+        nodes_drawing.set_edgecolor('black')
+        nodes_drawing = nx.draw_networkx_nodes(G, pos, nodelist=self.get_nodes() - self.get_terminals(),
+                                               node_color=nonterminal_color)
+        nodes_drawing.set_edgecolor('black')
+
+        nx.draw_networkx_edges(G, pos)
+
+        edge_labels = nx.get_edge_attributes(G, 'tu')
+        nx.draw_networkx_edge_labels(G, pos,
+                                     edge_labels=edge_labels,
+                                     label_pos=0.1,
+                                     rotate=False)
+
+        edge_labels = nx.get_edge_attributes(G, 'tv')
+        nx.draw_networkx_edge_labels(G, pos,
+                                     edge_labels=edge_labels,
+                                     label_pos=0.9,
+                                     rotate=False)
+
+        plt.axis('off')
+
+        if save is not None:
+            plt.savefig(save)
+
+        plt.show()
+
