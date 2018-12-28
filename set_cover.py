0b9e14c014c5cf7ab160c4dfb8bbb94021553372
Ohad Cohen <ohadc3@gmail.com>
2018-12-28 14:47:51 +0200

    Replaced set covers

--- set_cover.py
+++ set_cover.py
@@ -1,43 +1,4 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+from graph_factory.edge_thresholds import create_facility_location
 
-G = nx.Graph()
-
-facilities = 'ABC'
-facilities_weights = (1, 1, 1)
-clients = map(str, range(1, 6))
-
-for f, w in zip(facilities, facilities_weights):
-    G.add_node(f, weight=w)
-for c in clients:
-    G.add_node(c, weight=0)
-
-G.add_edges_from(('1A', '3A', '3C'), weight=0)
-G.add_edges_from(('1B', '2A', '4C', '5C'), weight=0)
-G.add_edges_from(('2B', '4B', '5B'), weight=0)
-G.add_edges_from(('2C', '4A'), weight=0)
-G.add_edges_from(('3B',), weight=0)
-
-pos = nx.bipartite_layout(G, facilities)
-
-nodes = nx.draw_networkx_nodes(G, pos, nodelist=facilities, node_color='w')
-nodes.set_edgecolor('black')
-nodes = nx.draw_networkx_nodes(G, pos, nodelist=clients, node_color='r')
-nodes.set_edgecolor('black')
-
-edges = nx.draw_networkx_edges(G, pos)
-
-for facility in facilities:
-    plt.text(pos[facility][0] + 0.02, pos[facility][1] + 0.04, G.nodes[facility]['weight'])
-
-edge_labels = nx.get_edge_attributes(G, 'weight')
-nx.draw_networkx_edge_labels(G, pos,
-                             edge_labels=edge_labels,
-                             label_pos=0.15,
-                             rotate=False)
-plt.axis('off')
-
-plt.savefig("output/set_cover.png")
-
-plt.show()
-
+graph = create_facility_location(3, 5, (1, 2), (0, 1), -7)
+graph.draw(tu=0.03, save="output/set_cover.png")
